e65fc4879c3d56913fd337138d4989ef09088abe
Bee <pizzaman8099@gmail.com>
2020-04-04 18:18:25 -0700

    Create the project

---
+++ src/Dothask.hs
@@ -0,0 +1,15 @@
+{- |
+Copyright: (c) 2020 Bee Ellis
+SPDX-License-Identifier: MIT
+Maintainer: Bee Ellis <pizzaman8099@gmail.com>
+
+Dotfile setup automation written in Haskell
+-}
+
+module Dothask
+       ( someFunc
+       ) where
+
+
+someFunc :: IO ()
+someFunc = putStrLn ("someFunc" :: String)
