633bcc8fe0af206ff7a0569fe4a4d92857fd15eb
Bee <pizzaman8099@gmail.com>
2020-04-16 17:09:46 -0700

    Revise type of Config objects add LinkConfig type

--- src/Config.hs
+++ src/Config.hs
@@ -1,25 +1,35 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DeriveGeneric #-}
 
 module Config (parseConfig) where
 
 import qualified Data.Yaml as Y
-import Data.Yaml (FromJSON(..), (.:))
+import Data.Yaml (FromJSON(..))
 import Data.HashMap.Strict as HM
+import GHC.Generics
+
+data LinkConfig =
+  LinkConfig {
+    create   :: Bool -- Create parent directories as needed (default: null)
+  , path     :: FilePath -- The source of the symlink (default: false)
+  , relink   :: Bool -- Remove the old target if it is a symlink (default: false)
+  , force    :: Bool -- Force removal of old target and create new link (default: false)
+  , relative :: Bool -- Use relative path to the source (default: false, absolute links)
+  } deriving (Generic, Show)
+
+instance FromJSON LinkConfig
 
 data Config =
   Config {
     defaults  :: Y.Object
-  , link      :: HM.HashMap FilePath (Maybe FilePath)
-  } deriving stock (Eq, Show)
+  , link      :: HM.HashMap FilePath (Maybe LinkConfig)
+  } deriving (Generic, Show)
 
-instance FromJSON Config where
-  parseJSON (Y.Object v) = Config
-    <$> v .: "defaults"
-    <*> v .: "link"
-  parseJSON _ =
-    Y.prependFailure "parsing Config failed, "
-      (typeMismatch "Object" invalid)
+instance FromJSON Config
+
+-- TODO: Create setDefaults function
+-- setDefaults should use Data.Yaml (.!=) to set missing default values
 
 -- It may make sense for the file path to be passed in as an argument so that the user
 -- can pass in a custom config path
@@ -27,7 +37,5 @@ parseConfig :: String -> IO Config
 parseConfig p = do
   -- res is the type of (MonadIO FromJSON)
   res <- Y.decodeFileThrow p
-  -- This doesn't work becauce both the functions passed into either need to
-  -- return the same type
   return (res :: Config)
 
