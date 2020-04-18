16741dec55f248d381e2a0b2066b10abdc4dece4
Bee <pizzaman8099@gmail.com>
2020-04-18 14:06:27 -0700

    Add test_dir to gitignore and start buildLink

--- src/Config.hs
+++ src/Config.hs
@@ -8,6 +8,9 @@ import qualified Data.Yaml as Y
 import Data.Yaml (FromJSON(..))
 import Data.HashMap.Strict as HM
 import GHC.Generics
+import System.Directory (pathIsSymbolicLink, doesPathExist)
+import Turtle
+import Prelude hiding (FilePath)
 
 data LinkConfig =
   LinkConfig {
@@ -31,6 +34,29 @@ instance FromJSON Config
 
 -- TODO: Create setDefaults function
 -- setDefaults should use Data.Yaml (.!=) to set missing default values
+
+-- | Clean target if needed
+removeFile :: FilePath -> IO ()
+removeFile pth
+  | not testfile pth =
+  | otherwise = 
+
+-- | Build symlink for LinkConfig datatype
+-- | Need to negotiate LinkConfig w/ set defaults
+buildLink :: FilePath -> LinkConfig -> IO ()
+buildLink trg (LinkConfig {create = c, path = src, relink = rln, force = f, relative = rel})
+  | (not rln) && (doesPathExist trg) && (pathIsSymbolicLink) = -- Print link already exist
+  | (not f) && (doesPathExist trg) = -- Return IO error
+  -- I want to recursively call this function but I would need to check if the
+  -- file tree has been created on each successive call which would degrade
+  -- preformance. I think to proceed here this mktree will have to be moved
+  -- into an IO do block
+  | otherwise = do
+    if c
+       then mktree $ directory src >> buildLink 
+       else if (
+    -- symlink fails if the file already exists
+    symLink $
 
 -- | Parse config file into Config object
 parseConfig :: String -> IO Config
