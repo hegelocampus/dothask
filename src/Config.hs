aafbaefe7808d8fd26038274974529f7376a7afd
Bee <pizzaman8099@gmail.com>
2020-04-22 15:34:49 -0700

    Implement cleanTarget to wrap clean functions

--- src/Config.hs
+++ src/Config.hs
@@ -8,8 +8,7 @@ import qualified Data.Yaml as Y
 import Data.Yaml (FromJSON(..))
 import Data.HashMap.Strict as HM
 import GHC.Generics
-import System.Directory (pathIsSymbolicLink, doesPathExist)
-import Turtle
+import Turtle hiding (relative)
 import Prelude hiding (FilePath)
 
 data LinkConfig =
@@ -36,17 +35,21 @@ instance FromJSON Config
 -- setDefaults should use Data.Yaml (.!=) to set missing default values
 
 -- | Clean target symlink if needed
-cleanTargetLink :: FilePath -> Bool -> IO ()
-cleanTargetLink pth reln
-  | isSymbolicLink pth && reln = rm pth >> stdout ("Removing existing symlink: " ++ pth)
-  | isSymbolicLink pth && not reln = ioError $ "symlink " ++ pth ++ " already exists!"
+cleanTargetLink :: FilePath -> IO ()
+cleanTargetLink pth
+  | isSymbolicLink pth = rm pth >> stdout ("Removing existing symlink: " ++ pth)
   | otherwise = stdout ("Target " ++ pth ++ " is already clean.")
 
 -- | Clean target file if needed
 cleanTargetFile :: FilePath -> IO ()
-cleanTargetFile pth 
+cleanTargetFile pth
   | testfile pth = rm pth >> stdout ("Removing existing file: " ++ pth)
   | otherwise = stdout ("Target " ++ pth ++ " is clean.")
+
+cleanTarget :: Bool -> Bool -> FilePath -> IO ()
+cleanTarget True _ pth = cleanTargetFile pth
+cleanTarget _ True pth = cleanTargetLink pth
+cleanTarget _ _ pth    = stdout ("Assuming target path is clean: " ++ pth)
 
 -- | Check that the tree exists and if it can be created
 checkTree :: FilePath -> Bool -> IO ()
@@ -65,14 +68,14 @@ checkTree path allowed
 -- | remainder of the symlinks.
 buildLink :: FilePath -> LinkConfig -> IO ()
 buildLink trg (LinkConfig {create = c, path = src, relink = rln, force = f, relative = rel})
-  --| (not rln) && (doesPathExist trg) && (pathIsSymbolicLink) = -- Print link already exist
-  --| (not f) && (doesPathExist trg) = -- Return IO error
+  -- (not rln) && (doesPathExist trg) && (pathIsSymbolicLink) = -- Print link already exist
+  -- (not f) && (doesPathExist trg) = -- Return IO error
   | otherwise = do
     -- Check that dirtree exits
     checkTree trg c
     -- Handle existing files
     if f then cleanTargetFile trg else cleanTargetLink trg rln
-    symLink $ src trg 
+    symlink $ src trg 
 
 -- | Parse config file into Config object
 parseConfig :: String -> IO Config
