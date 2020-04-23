42369dbcc79ce14d98db46cd9a21fb5d37b00e90
Bee <pizzaman8099@gmail.com>
2020-04-23 14:41:02 -0700

    Update clean target to raise error in case of collisions

--- src/Config.hs
+++ src/Config.hs
@@ -36,53 +36,61 @@ instance FromJSON Config
 
 -- | Clean target symlink if needed
 cleanTargetLink :: FilePath -> IO ()
-cleanTargetLink pth
-  | tstpth && isNotSymbolicLink pth = ioError (
-      format ("File already exists at:"%fp%"!\n") pth)
-  | tstpth = rm pth >> printf ("Removing existing symlink: "%fp%"\n") pth
-  | otherwise = printf ("Target "%fp%" is already clean.\n") pth
-  where tstpth = testfile pth
+cleanTargetLink pth = do
+  tstpth <- testfile pth
+  if tstpth
+    then
+      isNotSymbolicLink pth >>= \isLn -> if isLn
+        then
+          ioError (
+            format ("File already exists at:"%fp%"!\n") pth)
+        else
+          rm pth >> printf ("Removing existing symlink: "%fp%"\n") pth
+    else
+      printf ("Target "%fp%" is already clean.\n") pth
 
 -- | Clean target file if needed
 cleanTargetFile :: FilePath -> IO ()
-cleanTargetFile pth
-  | testfile pth = rm pth >> printf ("Removing existing file: "%fp%"\n") pth
-  | otherwise = printf ("Target "%fp%" is clean.\n") pth
+cleanTargetFile pth = testfile pth >>= \exists ->
+  if exists
+    then rm pth >> printf ("Removing existing file: "%fp%"\n") pth
+    else printf ("Target "%fp%" is already clean.\n") pth
 
 cleanTarget :: Bool -> Bool -> FilePath -> IO ()
 cleanTarget True _ pth = cleanTargetFile pth
 cleanTarget _ True pth = cleanTargetLink pth
-cleanTarget _ _ pth    = printf ("Assuming target path is clean: "%fp%"\n") pth
+-- It may be wise to actually check this
+-- and raise an error if the dirtree does not exist
+cleanTarget _ _ pth    = testfile pth >>= \exists ->
+  if exists
+     then ioError $ format ("Filepath is not clean!\n"%fp%" already exists!\n") pth
+     else printf ("Path is clean: "%fp%"\n") pth
 
 -- | Check that the tree exists and if it can be created
 checkTree :: FilePath -> Bool -> IO ()
 checkTree pth canMake
-  | canMake = mktree dirpath >> printf ("Created directory: "%fp%"\n") dirpath
-  | testdir dirpath = printf (fp%" already exists!\n") pth
-  | otherwise = ioError $ format ("Directory "%fp%" does not exist!\n") dirpath
+    | canMake = mktree dirpath >> printf ("Created directory: "%fp%"\n") dirpath
+    | testdir dirpath = printf (fp%" already exists!\n") pth
+    | otherwise = ioError $ format ("Directory "%fp%" does not exist!\n") dirpath
   where dirpath = dropExtension pth
 
 -- NOTE: symlink fails if the file already exists
 
+-- TODO: Need to negotiate LinkConfig w/ set defaults
 -- | Build symlink for LinkConfig datatype
--- | Need to negotiate LinkConfig w/ set defaults
--- | This Function will raise an IO error if it is unable to create the
--- | symlink, this exception should !not! disrupt the execution of the
--- | remainder of the symlinks.
+-- | This Function will raise an IO error if it is unable to create the symlink.
 buildLink :: FilePath -> LinkConfig -> IO ()
-buildLink pth LinkConfig {create = c, path = src, relink = rln, force = f,
-                           relative = rel}
-  =  do
-    -- Check that dirtree exits
-    checkTree pth c
-    -- Handle existing files
-    cleanTarget f rln pth
-    symlink src pth
+buildLink pth LinkConfig {
+                create = c
+              , path = src
+              , relink = rln
+              , force = f
+              , relative = rel }
+    = checkTree pth c
+    >> cleanTarget f rln pth
+    >> symlink src pth
 
 -- | Parse config file into Config object
 parseConfig :: String -> IO Config
-parseConfig p = do
-  -- res is the type of (MonadIO FromJSON)
-  res <- Y.decodeFileThrow p
-  return (res :: Config)
+parseConfig p = Y.decodeFileThrow p >>= \res -> return (res :: Config)
 
