a3dfd9e1a1eebc2cf60c2caa600ad7fa2692a534
Bee <pizzaman8099@gmail.com>
2020-04-23 17:54:57 -0700

    Move bulk of functions from Config to Dothask

--- src/Config.hs
+++ src/Config.hs
@@ -1,8 +1,23 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
 
-module Config (parseConfig) where
+module Config
+    (
+      parseConfig
+    , LinkConfig
+        (
+          create
+        , path
+        , relink
+        , force
+        , relative
+        )
+    , Config
+        (
+          defaults
+        , link
+        )
+    ) where
 
 -- TODO: Move most of this function into the dothask.hs file, all that should
 -- be happening here is the initiall Parsing of the config and setting of
@@ -10,95 +25,31 @@ module Config (parseConfig) where
 -- Can then import the LinkConfig and Config types into the main file for meat
 -- and potatoes actions
 
-import Turtle hiding (relative)
-import Prelude hiding (FilePath)
 import qualified Data.Yaml as Y
-import qualified Data.Text as T
 --import Data.Yaml (FromJSON(..))
-import Data.HashMap.Strict as HM
+import qualified Data.HashMap.Strict as HM
 import GHC.Generics
 
-data LinkConfig =
-  LinkConfig {
-    create   :: Bool -- Create parent directories up to target (default: null)
-  , path     :: FilePath -- The source of the symlink (default: false)
-  , relink   :: Bool -- Remove the old target if it is a symlink (default: false)
-  , force    :: Bool -- Force removal of old target and create new link (default: false)
-  , relative :: Bool -- Use relative path to the source (default: false, absolute links)
-  } deriving (Generic, Show)
+data LinkConfig = LinkConfig
+  {
+    create   :: !Bool      -- ^ Create parent directories up to target (default: null)
+  , path     :: !FilePath  -- ^ The source of the symlink (default: false)
+  , relink   :: !Bool      -- ^ Remove target if it is a symlink (default: false)
+  , force    :: !Bool      -- ^ Force removal of target (default: false)
+  , relative :: !Bool      -- ^ Use relative path to source (default: false)
+  } deriving stock (Generic, Show)
 
 instance Y.FromJSON LinkConfig
 
 -- TODOMAYBE: Move type definitions to their own files to facilitate use
-data Config =
-  Config {
-    defaults  :: Y.Object
-  , link      :: HM.HashMap FilePath (Maybe LinkConfig)
-  } deriving (Generic, Show)
+data Config = Config
+  {
+    defaults  :: !Y.Object
+  , link      :: !(HM.HashMap FilePath (Maybe LinkConfig))
+  } deriving stock (Generic, Show)
 
 instance Y.FromJSON Config
 
--- TODO: Create setDefaults function
--- setDefaults should use Data.Yaml (.!=) to set missing default values
-
--- | Create error message from Text
---raiseErrorF :: Format Text r -> b
-raiseErrorF txt = error . T.unpack . format txt
-
--- | Clean target symlink if needed
-cleanTargetLink :: FilePath -> IO ()
-cleanTargetLink pth = do
-  tstpth <- testfile pth
-  if tstpth
-    then
-      isNotSymbolicLink pth >>= \isLn -> if isLn
-        then
-          raiseErrorF ("File already exists at: " % fp % "\n") pth
-        else
-          rm pth >> printf ("Removing existing symlink: " % fp % "\n") pth
-    else
-      printf ("Target "%fp%" is already clean.\n") pth
-
--- | Clean target file if needed
-cleanTargetFile :: FilePath -> IO ()
-cleanTargetFile pth = testfile pth >>= \exists ->
-  if exists
-    then rm pth >> printf ("Removing existing file: "%fp%"\n") pth
-    else printf ("Target "%fp%" is already clean.\n") pth
-
-cleanTarget :: Bool -> Bool -> FilePath -> IO ()
-cleanTarget True _ pth = cleanTargetFile pth
-cleanTarget _ True pth = cleanTargetLink pth
--- It may be wise to actually check this
--- and raise an error if the dirtree does not exist
-cleanTarget _ _ pth    = testfile pth >>= \exists ->
-  if exists
-     then raiseErrorF ("Filepath is not clean!\n"%fp%" already exists!\n") pth
-     else printf ("Path is clean: "%fp%"\n") pth
-
--- | Check that the tree exists and if it can be created
-checkTree :: FilePath -> Bool -> IO ()
-checkTree pth True = mktree pth >> printf ("Created directory: "%fp%"\n") pth
-checkTree pth _ = testdir pth >>= \dirExists ->
-      if dirExists
-         then printf (fp%" already exists\n") pth
-         else raiseErrorF ("Directory "%fp%" does not exist!\n") pth
-
--- NOTE: symlink fails if the file already exists
-
--- TODO: Need to negotiate LinkConfig w/ set defaults
--- | Build symlink for LinkConfig datatype
--- | This Function will raise an IO error if it is unable to create the symlink.
-buildLink :: FilePath -> LinkConfig -> IO ()
-buildLink pth LinkConfig {
-                create = c
-              , path = src
-              , relink = rln
-              , force = f
-              , relative = rel }
-    = checkTree (dropExtension pth) c
-    >> cleanTarget f rln pth
-    >> symlink src pth
 
 -- | Parse config file into Config object
 parseConfig :: String -> IO Config
--- src/Dothask.hs
+++ src/Dothask.hs
@@ -10,12 +10,77 @@ Dotfile setup automation written in Haskell
 
 module Dothask (buildDots) where
 
+import qualified Data.Text as T
 import Data.Yaml ((.:))
-import Config
+import Turtle hiding (relative)
+import Prelude hiding (FilePath)
+
+import Config (parseConfig, LinkConfig (..), Config (..))
+
+-- TODO: Create setDefaults function
+-- setDefaults should use Data.Yaml (.!=) to set missing default values
+
+-- | Create error message from Format.
+--raiseErrorF :: Format Text r -> b
+raiseErrorF txt = error . T.unpack . format txt
+
+-- | Clean target symlink if needed.
+cleanTargetLink :: FilePath -> IO ()
+cleanTargetLink pth = do
+  tstpth <- testfile pth
+  if tstpth
+    then
+      isNotSymbolicLink pth >>= \isLn -> if isLn
+        then
+          raiseErrorF ("File already exists at: " % fp % "\n") pth
+        else
+          rm pth >> printf ("Removing existing symlink: " % fp % "\n") pth
+    else
+      printf ("Target "%fp%" is already clean.\n") pth
+
+-- | Clean target file if needed.
+cleanTargetFile :: FilePath -> IO ()
+cleanTargetFile pth = testfile pth >>= \exists ->
+  if exists
+    then rm pth >> printf ("Removing existing file: "%fp%"\n") pth
+    else printf ("Target "%fp%" is already clean.\n") pth
+
+-- | Clean target if allowed, raise error if file exists and cleaning is not allowed.
+cleanTarget :: Bool -> Bool -> FilePath -> IO ()
+cleanTarget True _ pth = cleanTargetFile pth
+cleanTarget _ True pth = cleanTargetLink pth
+cleanTarget _ _ pth    = testfile pth >>= \exists ->
+  if exists
+     then raiseErrorF ("Filepath is not clean!\n"%fp%" already exists!\n") pth
+     else printf ("Path is clean: "%fp%"\n") pth
+
+-- | Check that the tree exists and if it can be created.
+checkTree :: FilePath -> Bool -> IO ()
+checkTree pth True = mktree pth >> printf ("Created directory: "%fp%"\n") pth
+checkTree pth _ = testdir pth >>= \dirExists ->
+      if dirExists
+         then printf (fp%" already exists\n") pth
+         else raiseErrorF ("Directory "%fp%" does not exist!\n") pth
+
+-- NOTE: symlink fails if the file already exists
+
+-- TODO: Need to negotiate LinkConfig w/ set defaults
+-- | Build symlink for LinkConfig datatype. This Function will raise an
+-- IO error if it is unable to create the symlink given the LinkConfig's
+-- options.
+buildLink :: FilePath -> LinkConfig -> IO ()
+buildLink pth LinkConfig {
+                create = c
+              , path = src
+              , relink = rln
+              , force = f
+              , relative = rel }
+    = checkTree (dropExtension pth) c
+    >> cleanTarget f rln pth
+    >> symlink src pth
 
 buildDots :: String -> Bool -> IO ()
 buildDots configPath isQuiet = do
   res <- parseConfig configPath
   print res
-
 
