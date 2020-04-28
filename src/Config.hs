92f7d2df7612fdcb0cca0bfa56918342d1fbdec2
Bee <pizzaman8099@gmail.com>
2020-04-27 20:40:26 -0700

    Switch back to using LinkConfig record, add merge methods

--- src/Config.hs
+++ src/Config.hs
@@ -22,11 +22,11 @@ data DefaultsConfig = DefaultsConfig
 instance Y.FromJSON DefaultsConfig
 
 data LinkConfig = LinkConfig
-    { create   :: !(Maybe Bool)    -- ^ Create parent dirs (default: false)
-    , path     :: !(Maybe String)  -- ^ The source of the link (default: false)
-    , relink   :: !(Maybe Bool)    -- ^ Remove target symlink (default: false)
-    , force    :: !(Maybe Bool)    -- ^ Force removal of target (default: false)
-    , relative :: !(Maybe Bool)    -- ^ Relative path to source (default: false)
+    { createCfg   :: !(Maybe Bool)    -- ^ Create parent dirs (default: false)
+    , pathCfg     :: !(Maybe String)  -- ^ The source of the link (default: false)
+    , relinkCfg   :: !(Maybe Bool)    -- ^ Remove target symlink (default: false)
+    , forceCfg    :: !(Maybe Bool)    -- ^ Force removal of target (default: false)
+    , relativeCfg :: !(Maybe Bool)    -- ^ Relative path to source (default: false)
     } deriving stock (Generic, Show)
 
 instance Y.FromJSON LinkConfig
@@ -34,27 +34,38 @@ instance Y.FromJSON LinkConfig
 -- | Combine two Links where values in the first that are Nothing are replaced
 -- with the value in the second.
 -- Uses Data.HashMap.Strict.unionWith.
---weightedUnion :: HM.HashMap a Maybe b -> HM.HashMap a c -> HM.HashMap
-weightedUnion = HM.unionWith clearLNothings
-    where clearLNothings x y = if isJust x then x else y
+weightedUnion :: LinkConfig -> LinkConfig -> LinkConfig
+weightedUnion x y = LinkConfig
+    { createCfg   = leftIfMaybe (createCfg y) $ createCfg x
+    , pathCfg     = leftIfMaybe (pathCfg y) $ pathCfg x
+    , relinkCfg   = leftIfMaybe (relinkCfg y) $ relinkCfg x
+    , forceCfg    = leftIfMaybe (forceCfg y) $ forceCfg x
+    , relativeCfg = leftIfMaybe (relativeCfg y) $ relativeCfg x
+    }
+  where leftIfMaybe v1 v2 = if isJust v1 then v1 else v2
 
-unionS :: Y.Object -> StrictLink -> StrictLink
-unionS = HM.unionWith setPure
-    where setPure cv sv = fromMaybe sv cv
+removeMaybes :: LinkConfig -> StrictLink
+removeMaybes x = StrictLink
+    { create = fromMaybe False $ createCfg x
+    , path = fromMaybe "" $ pathCfg x
+    , relink = fromMaybe False $ relinkCfg x
+    , force = fromMaybe False $ forceCfg x
+    , relative = fromMaybe False $ relativeCfg x
+    }
 
 data StrictLink = StrictLink
-    { createCfg   :: !Bool    -- ^ Create parent dirs (default: false)
-    , pathCfg     :: !String  -- ^ The source of the link (default: false)
-    , relinkCfg   :: !Bool    -- ^ Remove target symlink (default: false)
-    , forceCfg    :: !Bool    -- ^ Force removal of target (default: false)
-    , relativeCfg :: !Bool    -- ^ Relative path to source (default: false)
+    { create   :: !Bool    -- ^ Create parent dirs (default: false)
+    , path     :: !String  -- ^ The source of the link (default: false)
+    , relink   :: !Bool    -- ^ Remove target symlink (default: false)
+    , force    :: !Bool    -- ^ Force removal of target (default: false)
+    , relative :: !Bool    -- ^ Relative path to source (default: false)
     } deriving stock (Generic, Show)
 
 -- | TODO Set defaults to be Maybe DefaultsConfig.
 data ConfigObj = ConfigObj
     { defaults :: !DefaultsConfig                                 -- ^ Defaults config object
     -- Order of operations
-    , link     :: !(HM.HashMap FilePath (Maybe (Either String Y.Object)))  -- ^ Link configuration
+    , link     :: !(HM.HashMap FilePath (Maybe (Either String LinkConfig)))  -- ^ Link configuration
     } deriving stock (Generic, Show)
 
 instance Y.FromJSON ConfigObj
