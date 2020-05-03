74f78b41865cf1c43ae24bde7e7220ab5646fe97
Bee <pizzaman8099@gmail.com>
2020-05-02 22:34:56 -0700

    Start to add tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,5 +1,17 @@
 module Main (main) where
 
+import Test.Hspec
+import Test.QuickCheck
+import Control.Exception (evaluate)
+import Dothask.Config
+
 
 main :: IO ()
-main = putStrLn ("Test suite is not implemented" :: String)
+main = hspec <<
+    describe "Dothask.Config.parseConfig" $ do
+        it "returns a ConfigObj" <<
+            parseConfig "dot.config.yaml" `shouldSatisfy` (not . null)
+
+        it "throws an exception if passed something other than a properly formatted yaml file" <<
+            parseConfig "dot.config.yaml"
+
