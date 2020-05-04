558132cc51e2b83842c06ee811c8cb982ff1c0e7
Bee <pizzaman8099@gmail.com>
2020-05-03 21:12:07 -0700

    Fix tests TODO: Implement Eq for Config

--- test/Spec.hs
+++ test/Spec.hs
@@ -3,15 +3,18 @@ module Main (main) where
 import Test.Hspec
 import Test.QuickCheck
 import Control.Exception (evaluate)
-import Dothask.Config
+import qualified Data.HashMap.Strict as HM
+import Dothask.Config hiding (empty)
+import qualified Dothask.Config as C (empty)
 
 
 main :: IO ()
 main = hspec $ do
     describe "Dothask.Config.parseConfig" $ do
-        it "returns a ConfigObj" $ do
-            parseConfig "dot.config.yaml" `shouldSatisfy` (not . null)
+        it "does not return an empty ConfigObj" $ do
+            parseConfig "dot.config.yaml" `shouldNotReturn` C.empty
 
-        it "throws an exception if passed something other than a properly formatted yaml file" $ do
-            parseConfig "not.a.yaml" `shouldThrow` anyException
+        context "When passed something other than a yaml file" $ do
+            it "throws an exception" $ do
+                parseConfig "not.a.yaml" `shouldThrow` anyException
 
